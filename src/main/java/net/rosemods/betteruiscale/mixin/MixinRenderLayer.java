a7c0129e9fd62a1b71cf0226edd433d56b2d04d0
Oribuin <oribuinn@gmail.com>
2025-02-16 16:25:07 +0000

    fix initial crashing

--- src/main/java/net/rosemods/betteruiscale/mixin/MixinRenderLayer.java
+++ src/main/java/net/rosemods/betteruiscale/mixin/MixinRenderLayer.java
@@ -10,13 +10,15 @@ import org.spongepowered.asm.mixin.injection.Slice;
 @Mixin(RenderLayer.class)
 public class MixinRenderLayer {
     @ModifyConstant(
-        method = "method_34834",
-        constant = @Constant(intValue = 0, ordinal = 0, log = true),
-        slice = @Slice(
-            from = @At(value = "INVOKE", target = "net/minecraft/client/render/RenderPhase$Texture.<init>(Lnet/minecraft/util/Identifier;ZZ)V", ordinal = 0, shift = At.Shift.BY, by = -2),
-            to = @At(value = "INVOKE", target = "net/minecraft/client/render/RenderPhase$Texture.<init>(Lnet/minecraft/util/Identifier;ZZ)V", ordinal = 0)
-        ))
+            method = "method_34834",
+            constant = @Constant(intValue = 0, ordinal = 0, log = true),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderPhase$Texture;<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/util/TriState;Z)V", ordinal = 0, shift = At.Shift.BY, by = -2),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderPhase$Texture;<init>(Lnet/minecraft/util/Identifier;Lnet/minecraft/util/TriState;Z)V", ordinal = 0)
+            ))
+
     private static int enableBilinear(int bool) {
         return 1;
     }
+
 }
